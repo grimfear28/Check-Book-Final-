5d6b4593a6e744355e98f69a361ac217e16e6cde
Luke Sanders <ls0057@sunysullivan.edu>
2018-05-14 20:42:39 -0400

    Made it work.

--- Check Book/Check Book/obj/Release/android/check_book/check_book/R.java
+++ Check Book/Check Book/obj/Release/android/check_book/check_book/R.java
@@ -14,17 +14,20 @@ public final class R {
         public static final int plus=0x7f020000;
     }
     public static final class id {
-        public static final int btnAddCheck=0x7f05000a;
-        public static final int btnCreateRecord=0x7f050008;
-        public static final int progressBar1=0x7f050009;
-        public static final int textView1=0x7f050000;
-        public static final int textView2=0x7f050002;
-        public static final int textView3=0x7f050004;
-        public static final int textView4=0x7f050006;
-        public static final int txtAccountName=0x7f050001;
-        public static final int txtAccountNumber=0x7f050003;
-        public static final int txtAmount=0x7f050007;
-        public static final int txtDate=0x7f050005;
+        public static final int btnAddCheck=0x7f05000d;
+        public static final int btnCreateRecord=0x7f050002;
+        public static final int progressBar1=0x7f05000c;
+        public static final int scrollView1=0x7f05000a;
+        public static final int showInfoText=0x7f05000b;
+        public static final int textView1=0x7f050003;
+        public static final int textView2=0x7f050005;
+        public static final int textView3=0x7f050000;
+        public static final int textView4=0x7f050007;
+        public static final int textView5=0x7f050009;
+        public static final int txtAccountName=0x7f050004;
+        public static final int txtAccountNumber=0x7f050006;
+        public static final int txtAmount=0x7f050008;
+        public static final int txtDate=0x7f050001;
     }
     public static final class layout {
         public static final int checkadd=0x7f030000;
